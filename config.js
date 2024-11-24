0afc603d59a8e0570e5b4b9d8517efb5150ec448
oisaimrane <95139857+oisaimrane@users.noreply.github.com>
2024-11-24 13:08:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = {
   ownerID: ["793877159966015548", ""],
   botInvite: "",
   supportServer: "",
-  mongodbURL: "mongodb+srv://c4clan3:c4clan3@c4clan3.ufkpd.mongodb.net/?retryWrites=true&w=majority",
+  mongodbURL: "mongodb+srv://best:UDglXQEBZFuksePq@cluster0.68mktfu.mongodb.net/?retryWrites=true&w=majority",
   status: 'BAMBINO',
   commandsDir: './commands',
   language: "en",
